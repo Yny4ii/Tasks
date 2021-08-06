6b8b6b5d109dcdd0c0813fd89cb7cc98d144c69f
Yny4ii <71377999+Yny4ii@users.noreply.github.com>
2021-08-06 15:58:49 +0300

    Update Main.java

--- Task4/Main.java
+++ Task4/Main.java
@@ -1,18 +1,10 @@
 package Task4;
 
-
 public class Main {
-    public static void main(String[] args) {
-        Task4 task4 = new Task4(); // enter a number
-        task4.division();
+    public static void main(String[] args) throws IntegerException {
+        Task4 task4 = new Task4();
+        task4.division();// enter a number
 
     }
 }
-
-
-
-
-
-
-
 
