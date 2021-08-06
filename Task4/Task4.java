774b4ec92269d63bcbea17cf5b184458ebecef1f
Yny4ii <71377999+Yny4ii@users.noreply.github.com>
2021-08-06 15:58:17 +0300

    Update Task4.java

--- Task4/Task4.java
+++ Task4/Task4.java
@@ -1,32 +1,11 @@
 package Task4;
 
 public class Task4{
-    int n;
-    Task4(int n){
-        try{
-            if(n % 1 != 0){
-                throw new IntegerException();
-            }
-        }catch (IntegerException ex){
-            System.out.println("Exception: " + ex);
-            return;
-        }
-        this.n = n;
-
-    }
-    Task4(double n){
-        try{
-            if(n % 1 != 0){
-                throw new IntegerException();
-            }
-        }catch (IntegerException ex){
-            System.out.println("Exception: " + ex);
-            return;
-        }
-
+    public void division(Number n) throws IntegerException {
+        throw new IntegerException();
     }
 
-    public void division(){
+    public void division(int n){
         for(int i = 1; i <= n / 2; i++){
             if(n % i == 0){
                 System.out.println(i);
